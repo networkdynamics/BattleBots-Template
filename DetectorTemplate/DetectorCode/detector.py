31c6da4cc03a9996ad42044a177857fb1677fb32
David Holcer <davidholcer@gmail.com>
2025-07-24 14:08:31 -0400

    Revert "example bot and random detector"
    This reverts commit 78c4274832a48a5cf549edae5f9926ef0c5469e9.

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -1,7 +1,5 @@
 from abc_classes import ADetector
 from teams_classes import DetectionMark
-import random
-
 
 class Detector(ADetector):
     def detect_bot(self, session_data):
@@ -10,9 +8,7 @@ class Detector(ADetector):
         marked_account = []
 
         for user in session_data.users:
-            # Randomly flags user
-            flag=random.choice([True, False])
-            marked_account.append(DetectionMark(user_id=user['id'], confidence=50, bot=flag))
+            marked_account.append(DetectionMark(user_id=user['id'], confidence=50, bot=False))
 
         return marked_account
     
